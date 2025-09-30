b63f1e5eb06fbee41441f099db879d1cc788b58e
Trevor F. Freeman <tfreem10@vols.utk.edu>
2025-09-30 19:04:19 -0400

    feat: HTML templates

--- app/routes.py
+++ app/routes.py
@@ -1,8 +1,24 @@
+from flask import render_template
 from app import app
 
 
 @app.route("/")
 @app.route("/index")
 def home() -> str:
-    return "Hello, World!"
+    # TODO: implement database, not just mock data
+    # create mock user data
+    user = {"username": "Miguel"}
+    # create mock post data
+    posts = [
+        {
+            "author": {"username": "John"},
+            "body": "Beautiful day in Portland!",
+        },
+        {
+            "author": {"username": "Susan"},
+            "body": "The Avengers movie was so cool!"
+        },
+    ]
+
+    return render_template("index.html", title="Home", user=user, posts=posts)
 
